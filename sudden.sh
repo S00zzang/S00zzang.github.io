55dacd48d8787eb580c68d510c20891294c21cad
S00zzang <sujinpark8719561@gmail.com>
2024-07-11 10:22:40 +0900

    ㅋㅋ

---
+++ sudden.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+FILE=~/temp/_SUDDEN
+
+while true
+do
+
+if [[ -f "$FILE" ]]; then
+   	echo "check $FILE"
+	#else 
+	#clear
+	#date	##만일 파일이 없다면 date 반환
+fi
+	sleep 3
+
+done
+
+#if [-e FILE ]; then
+#echo "SUDDEN!!"
+#sleep 3
+#else
+#echo "not sudden . . ."
+#sleep 3
+#fi
+#done
+
+#echo "check"
+#sleep 3
+#done
+
+
